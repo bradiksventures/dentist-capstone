385bf996e1b193be8b4b2a4392b94be524bffe66
Dian <dian@reviewbuzz.com>
2023-04-14 18:58:29 +0800

    checkin commit: dentist availability.

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -6,18 +6,25 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 gap-4 grid md:grid-cols-2 ">
+            <div class="space-y-4">
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg mb">
                     @include('profile.partials.update-profile-information-form')
                 </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
+
+            <div>
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    @include('profile.partials.update-dentist-availability')
+                </div>
+            </div>
+
+
+
+
         </div>
     </div>
 </x-app-layout>
